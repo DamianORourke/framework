9e22ac29525c83b14235dfcd034f022297aa67d1
DamianORourke <dorourk4@tcd.ie>
2022-02-21 08:47:25 +0000

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+var browserSync = require('browser-sync').create();
+
+gulp.task('hello', function(){
+    console.log('hello');
+});
+
+gulp.task('sass', function(){
+    return gulp.src('app/assets/sass/newStyles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('watch', gulp.series('sass', function(){
+
+    browserSync.init({
+        server: "./app/"
+    });
+    
+    gulp.watch('app/assets/sass/newStyles.scss', gulp.series('sass'));
+    gulp.watch('app/*.html').on('change', browserSync.reload);
+    gulp.watch('app/js/*js').on('change', browserSync.reload);
+}));
+
